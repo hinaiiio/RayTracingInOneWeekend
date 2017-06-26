e3bf5c5655e1b50cc7d43bec6b076cdcc33ca914
Hina <bob.piyo.1995@gmail.com>
2017-06-26 18:49:04 +0900

    Initial Commit

---
+++ RayTracingInOneWeekend/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RayTracingInOneWeekend
+//
+//  Created by Hina on 2017/06/26.
+//  Copyright © 2017年 Hina. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
