059ac51b00169aa0563f3ecee8120f8b4ba1879f
Hina <bob.piyo.1995@gmail.com>
2017-07-18 13:38:35 +0900

    add vec3.hpp

--- RayTracingInOneWeekend/main.cpp
+++ RayTracingInOneWeekend/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include "vec3.hpp"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -15,12 +16,18 @@ int main(int argc, const char * argv[]) {
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
     for (int j = ny-1; j >= 0; j--){
         for(int i = 0; i<nx; i++){
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
-            float b = 0.2;
-            int ir = int(255.99*r);
-            int ig = int(255.99*g);
-            int ib = int(255.99*b);
+//            float r = float(i) / float(nx);
+//            float g = float(j) / float(ny);
+//            float b = 0.2;
+//            int ir = int(255.99*r);
+//            int ig = int(255.99*g);
+//            int ib = int(255.99*b);
+            
+            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+            int ir = int(255.99*col[0]);
+            int ig = int(255.99*col[1]);
+            int ib = int(255.99*col[2]);
+            
             std::cout << ir << " " << ig << " " << ib << "\n";
             
             
